41461a7f81c4322e3c1e92a86fe1967870ecd631
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-04-12 20:38:48 -0700

    rewriting state in App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
 import "./App.css";
 import React, { Component } from "react";
 import General from "./components/General";
-import Education from "./components/Education";
-import Professional from "./components/Professional";
-import CV from "./components/CV";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: {
+        text: "",
+      },
+      nameValue: [],
+      email: {
+        text: "",
+      },
+      emailValue: [],
+
+      phone: {
+        number: "",
+      },
+      phoneValue: [],
+    };
+
+    this.onSubmitForm = this.onSubmitForm.bind(this);
   }
 
+  handleName = (e) => {
+    this.setState({
+      name: {
+        text: e.target.value,
+      },
+    });
+  };
+  handleEmail = (e) => {
+    this.setState({
+      email: {
+        text: e.target.value,
+      },
+    });
+  };
+  handlePhone = (e) => {
+    this.setState({
+      phone: {
+        number: e.target.value,
+      },
+    });
+  };
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    this.setState({
+      nameValue: this.state.nameValue.concat(this.state.name.text),
+      name: { text: "" },
+      emailValue: this.state.emailValue.concat(this.state.email.text),
+      email: { text: "" },
+      phoneValue: this.state.phoneValue.concat(this.state.phone.number),
+      phone: { number: "" },
+    });
+  };
+
   render() {
+    const { name, nameValue, email, emailValue, phone, phoneValue } =
+      this.state;
     return (
       <div>
         <div className="container">
-          <General />
-          <Education />
-          <Professional />
+          <form onSubmit={this.onSubmitForm}>
+            <div className="Name">
+              <label>Name</label>
+              <input type="text" onChange={this.handleName} value={name.text} />
+            </div>
+
+            <div>
+              <label>E-mail</label>
+              <input
+                type="email"
+                onChange={this.handleEmail}
+                value={email.text}
+              />
+            </div>
+
+            <div>
+              <label>Phone Number</label>
+              <input
+                type="text"
+                onChange={this.handlePhone}
+                value={phone.number}
+              />
+            </div>
+            <div className="buttons">
+              <button type="submit" value="Submit">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-        <CV />
+
+        <div class="CV-Template">
+          <p>{nameValue}</p>
+          <p>{emailValue}</p>
+          <p>{phoneValue}</p>
+        </div>
       </div>
     );
   }
--- src/components/General.js
+++ src/components/General.js
@@ -1,92 +1,18 @@
 import React, { Component } from "react";
-import Render from "./Render";
-class General extends Component {
+
+export default class General extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "",
-      email: "",
-      number: "",
-
-      test: [],
-      test2: [],
-      test3: [],
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEmail = this.handleEmail.bind(this);
-    this.handleNumber = this.handleNumber.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-  handleEmail(event) {
-    this.setState({ email: event.target.value });
   }
 
-  handleNumber(event) {
-    this.setState({ number: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    this.setState({
-      test: this.state.test.concat(this.state.name),
-      test2: this.state.test2.concat(this.state.email),
-      test3: this.state.test3.concat(this.state.number),
-      name: "",
-      email: "",
-      number: "",
-    });
-  }
-
-  editForm() {
-    alert("you clicked the edit button!");
-  }
   render() {
-    const { test, test2, test3 } = this.state;
     return (
-      <div className="General">
-        <form onSubmit={this.handleSubmit}>
-          <div className="Name">
-            <label>Name</label>
-            <input
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div>
-            <label>E-mail</label>
-            <input
-              type="email"
-              value={this.state.email}
-              onChange={this.handleEmail}
-            />
-          </div>
-
-          <div>
-            <label>Phone Number</label>
-            <input
-              type="number"
-              value={this.state.number}
-              onChange={this.handleNumber}
-            />
-          </div>
-          <div className="buttons">
-            <button type="submit" value="Submit">
-              Submit
-            </button>
-            <button onClick={this.editForm}>Edit</button>
-          </div>
-        </form>
-        <Render test={test} test2={test2} test3={test3} />
+      <div>
+        <div className="Name">
+          <label>Name</label>
+          <input type="text" onChange={this.handleName} value={name.text} />
+        </div>
       </div>
     );
   }
 }
-
-export default General;
