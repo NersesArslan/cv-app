db575b761336faf8c174ec3cc28b197d8818f056
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-04-17 06:43:52 -0700

    Updates

---
+++ precache-manifest.eb8a499b4191d3d10a060025d6936484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "997eaecd80896ac164e8",
+    "url": "/cv-app/static/css/main.c2422394.chunk.css"
+  },
+  {
+    "revision": "997eaecd80896ac164e8",
+    "url": "/cv-app/static/js/main.997eaecd.chunk.js"
+  },
+  {
+    "revision": "4fd0c69d4a383c86b906",
+    "url": "/cv-app/static/js/1.4fd0c69d.chunk.js"
+  },
+  {
+    "revision": "5257d31460df784bef29",
+    "url": "/cv-app/static/js/runtime~main.5257d314.js"
+  },
+  {
+    "revision": "6bd4219b9969663f02de004790037402",
+    "url": "/cv-app/index.html"
+  }
+];
